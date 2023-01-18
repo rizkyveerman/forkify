be80d5d1c58f5176db7c1201d6f7a3be5546403b
rizky MSI <rizfirsy@yahoo.com>
2023-01-18 17:25:07 +0700

    api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'eefd3604-3d49-49ad-806a-f656893039fe';
 export const MODAL_CLOSE_SEC = 2.5;
